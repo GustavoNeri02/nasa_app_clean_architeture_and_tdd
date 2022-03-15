2a4134918bb1f5978bbc5d1328ff227bb3effe42
Gustavo Neri Pereira Dias <gutneri@hotmail.com>
2022-03-15 09:50:26 -0300

    🧪 Adicionando um Failure test de Use Case

--- lib/core/erros/failures.dart
+++ lib/core/erros/failures.dart
@@ -1,3 +1,9 @@
 import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {}
+
+//extensão da abstração
+class ServerFailure extends Failure {
+  @override
+  List<Object?> get props => [];
+}
--- test/features/domain/usecases/get_space_media_from_date_usecase_test.dart
+++ test/features/domain/usecases/get_space_media_from_date_usecase_test.dart
@@ -2,6 +2,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:nasa_app_clean_architeture_and_tdd/core/erros/failures.dart';
 import 'package:nasa_app_clean_architeture_and_tdd/features/domain/entities/space_media_entity.dart';
 import 'package:nasa_app_clean_architeture_and_tdd/features/domain/repositories/space_media_repository.dart';
 import 'package:nasa_app_clean_architeture_and_tdd/features/domain/usecases/get_space_media_from_date_usecase.dart';
@@ -35,13 +36,24 @@ void main() {
   final tDate = DateTime(2021, 02, 02);
 
   test('should get space media for a given date from the repository', () async {
+    // Act
     when(() => repository.getSpaceMediaFromDate(tDate))
         .thenAnswer((_) async => const Right(tSpaceMedia));
-
+    // Act
     final result = await usecase.call(tDate);
-
+    // Assert
     expect(result, const Right(tSpaceMedia));
+    verify(() => repository.getSpaceMediaFromDate(tDate)).called(1);
+  });
 
+  test('should return a Failure when don\'t succeed', () async {
+    // Arrange
+    when(() => repository.getSpaceMediaFromDate(tDate)).thenAnswer(
+        (_) async => Left<Failure, SpaceMediaEntity>(ServerFailure()));
+    // Act
+    final result = await usecase.call(tDate);
+    // Assert
+    expect(result, Left(ServerFailure()));
     verify(() => repository.getSpaceMediaFromDate(tDate)).called(1);
   });
 }
