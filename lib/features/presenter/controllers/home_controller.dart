f6016b5ad07a64fb87ffedd44a5b33d59efdf6ec
Gustavo Neri Pereira Dias <gutneri@hotmail.com>
2022-03-17 18:51:42 -0300

    ✅ add tests for the Flutter_Triple's controller

--- lib/features/presenter/controllers/home_controller.dart
+++ lib/features/presenter/controllers/home_controller.dart
@@ -4,29 +4,14 @@ import 'package:nasa_app_clean_architeture_and_tdd/core/erros/failures.dart';
 import 'package:nasa_app_clean_architeture_and_tdd/features/domain/entities/space_media_entity.dart';
 import 'package:nasa_app_clean_architeture_and_tdd/features/domain/usecases/get_space_media_from_date_usecase.dart';
 
-class HomeController extends Store<Failure, SpaceMediaEntity> {
+class HomeController extends StreamStore<Failure, SpaceMediaEntity> {
   final GetSpaceMediaFromDateUsecase usecase;
 
   HomeController(this.usecase)
       : super(const SpaceMediaEntity(
             description: "", mediaType: "", title: "", mediaUrl: ""));
 
-  @override
-  Future destroy() {
-    // TODO: implement destroy
-    throw UnimplementedError();
-  }
-
-  @override
-  Disposer observer(
-      {void Function(SpaceMediaEntity state)? onState,
-      void Function(bool isLoading)? onLoading,
-      void Function(Failure error)? onError}) {
-    // TODO: implement observer
-    throw UnimplementedError();
-  }
-
-  void getSpaceMediaFromDate(DateTime tDate) async {
+  getSpaceMediaFromDate(DateTime tDate) async {
     /*
     setLoading(true);
     final result = await usecase(tDate);
--- test/features/presenter/controllers/home_controller_test.dart
+++ test/features/presenter/controllers/home_controller_test.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:nasa_app_clean_architeture_and_tdd/core/erros/failures.dart';
 import 'package:nasa_app_clean_architeture_and_tdd/features/domain/usecases/get_space_media_from_date_usecase.dart';
 import 'package:nasa_app_clean_architeture_and_tdd/features/presenter/controllers/home_controller.dart';
 
@@ -21,9 +22,34 @@ void main() {
 
   test("should return a SpaceMedia from the usecase", () async {
     // Arrange
-    when(() => mockUsecase(any())).thenAnswer((_) async => Right(tSpaceMedia));
+    when(() => mockUsecase(any()))
+        .thenAnswer((_) async => const Right(tSpaceMedia));
     // Act
-    controller.getSpaceMediaFromDate(tDate);
+    await controller.getSpaceMediaFromDate(tDate);
     // Assert
+    controller.observer(
+        //success
+        onState: (state) {
+      expect(state, tSpaceMedia);
+      verify(() => mockUsecase(tDate)).called(1);
+    });
+  });
+
+  final tFailure = ServerFailure();
+
+  test("should return a Failure from the usecase when there is an error",
+      () async {
+    // Arrange
+    when(() => mockUsecase(any())).thenAnswer((_) async => Left(tFailure));
+    // Act
+    await controller.getSpaceMediaFromDate(tDate);
+    // Assert
+    controller.observer(
+      //error
+      onError: (error) async {
+        expect(error, tFailure);
+        verify(() => mockUsecase(tDate)).called(1);
+      },
+    );
   });
 }
